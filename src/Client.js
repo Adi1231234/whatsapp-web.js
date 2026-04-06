52ae3804e5d549d109ab66c11884cdf07cb41328
Adi Aharoni <adi1231234@gmail.com>
2026-04-06 22:19:13 +0300

    fix: use sync listener with chained promise for response caching
    replace waitForResponse (one-shot) with page.on('response') that
    fires on every navigation. The handler is sync (not async) so it
    does not return a Promise to EventEmitter. res.text() is chained
    with .then/.catch so rejections are always handled.
    
    the original async handler returned a Promise that EventEmitter
    ignored, causing unhandledRejection when CDP disconnected.

--- src/Client.js
+++ src/Client.js
@@ -2013,13 +2013,15 @@ class Client extends EventEmitter {
                 }
             });
         } else {
-            this.pupPage
-                .waitForResponse((res) => res.ok() && res.url() === WhatsWebURL)
-                .then((res) => res.text())
-                .then((indexHtml) => {
-                    this.currentIndexHtml = indexHtml;
-                })
-                .catch((_) => _);
+            this.pupPage.on('response', (res) => {
+                if (res.ok() && res.url() === WhatsWebURL) {
+                    res.text()
+                        .then((indexHtml) => {
+                            this.currentIndexHtml = indexHtml;
+                        })
+                        .catch((_) => _);
+                }
+            });
         }
     }
 
